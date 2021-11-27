94dbf5985af8d710d901953c90d42876a43683cc
mr.Shu <mr@shu.io>
2021-11-27 19:33:02 +0100

    add: First version of `extract-wos-article.py`
    * Add the first version of `extract-wos-article.py` script based on
      `playwright`.
    
    * Specify the required requirements in `requirements.txt`
    
    * Document how to run the script in `README.md`
    
    * Fixes #1
    
    Signed-off-by: mr.Shu <mr@shu.io>

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # steel-wok
 Exactly what it sounds like
+
+## Installation
+
+All the dependencies should be in `requirements.txt`. Hence, installation
+should be as easy as running
+
+    pip install -r requirements.txt
+
+Since many of the scripts included here use `playwright`, we suggest installing
+its dependencies too:
+
+    playwright install
+
+## Running
+
+Should be as easy as running
+
+    python extract-wos-article.py https://www.webofscience.com/wos/woscc/full-record/WOS:000690353600001
+
+Which would then produce output similar to
+
+    {'DOI': '10.1007/s11262-021-01866-5',
+     'article_no': '',
+     'authors': [{'names': 'Brona', 'surname': 'Brejova'},
+                 {'names': 'Kristina', 'surname': 'Borsova'},
+                 {'names': 'Viktoria', 'surname': 'Hodorova'},
+                 {'names': 'Viktoria', 'surname': 'Cabanova'},
+                 {'names': 'Lenka', 'surname': 'Reizigova'},
+                 {'names': 'Evan D.', 'surname': 'Paul'},
+                 {'names': 'Pavol', 'surname': 'Cekan'},
+                 {'names': 'Boris', 'surname': 'Klempa'},
+                 {'names': 'Jozef', 'surname': 'Nosek'},
+                 {'names': 'Tomas', 'surname': 'Vinar'}],
+     'eissn': '1572-994X',
+     'issn': '0920-8569',
+     'issue': '6',
+     'journal': 'VIRUS GENES',
+     'page': '556-560',
+     'pubdate': 'DEC 2021',
+     'publisher': 'SPRINGER, VAN GODEWIJCKSTRAAT 30, 3311 GZ DORDRECHT, '
+                  'NETHERLANDS',
+     'times_cited': '1',
+     'title': 'A SARS-CoV-2 mutant from B.1.258 lineage with increment H69/ '
+              'increment V70 deletion in the Spike protein circulating in Central '
+              'Europe in the fall 2020',
+     'volume': '57',
+     'year': '2021'}
+
+To debug how the script works (i.e. by not making the Chromium that's being
+started headless), you can specify the `--debug` option.
+
+Shuld you want to make use of a proxy, e.g. SOCKS proxy running on port `11000`
+on `localhost`, you can do so by specifying further options:
+
+    python extract-wos-article.py https://www.webofscience.com/wos/woscc/full-record/WOS:000690353600001 --debug --proxy-server socks://localhost:11000
---
+++ extract-wos-article.py
@@ -0,0 +1,125 @@
+import asyncio
+from playwright.async_api import async_playwright, Page
+import typer
+import re
+
+AUTHORS_SELECTOR = '.authors-div > .ng-star-inserted > span:nth-child(2) .value' # noqa
+TITLE_SELECTOR = '.title.text--large'
+JOURNAL_SELECTOR = '.journal-content .journal-content-row'
+SELECTORS = {
+    'journal': 'app-jcr-overlay',
+    'volume': '#FullRTa-volume',
+    'issue': '#FullRTa-issue',
+    'page': '#FullRTa-pageNo',
+    'DOI': '#FullRTa-DOI',
+    'times_cited': '#snCitationData .large-link-number:nth-of-type(1)',
+    'pubdate': '#FullRTa-pubdate',
+    'article_no': 'span#FullRTa-articleNumberLabel',
+}
+
+JOURNAL_SELECTORS_MAPPING = {
+    'ISSN': 'issn',
+    'eISSN': 'eissn',
+    'Current Publisher': 'publisher',
+}
+
+YEAR_REGEX = re.compile('.*([0-9]{4})')
+
+
+async def extract_text_from_selector(selector: str, page: Page,
+                                     default_value: str = ''):
+    selector = await page.query_selector(selector)
+    if not selector:
+        return default_value
+
+    return await selector.inner_text()
+
+
+async def main_extract(url: str,
+                       debug: bool = False,
+                       proxy_server: str = ''):
+
+    async with async_playwright() as p:
+        if debug:
+            if proxy_server:
+                proxy = {
+                    'server': proxy_server
+                }
+            else:
+                proxy = None
+
+            browser = await p.chromium.launch(
+                headless=False,
+                slow_mo=100,
+                proxy=proxy
+            )
+        else:
+            browser = await p.chromium.launch()
+
+        page = await browser.new_page()
+        await page.goto(url, wait_until="networkidle")
+
+        await page.click('#onetrust-accept-btn-handler')
+
+        outputs = {}
+
+        title = await page.query_selector(TITLE_SELECTOR)
+        title_text = await title.inner_html()
+
+        outputs['title'] = title_text
+
+        authors = await page.query_selector_all(AUTHORS_SELECTOR)
+
+        authors_list = []
+        for author in authors:
+            author_str = await author.inner_text()
+            surname, *names = author_str.split(', ')
+            authors_list.append({
+                'surname': surname,
+                'names': ''.join(names)
+            })
+
+        outputs['authors'] = authors_list
+
+        for selector in SELECTORS.keys():
+            text = await extract_text_from_selector(SELECTORS[selector],
+                                                    page)
+            outputs[selector] = text
+
+        journal_metdata = {}
+        journal_content_rows = await page.query_selector_all(JOURNAL_SELECTOR)
+        for row in journal_content_rows:
+            key_selector = await row.query_selector('h3')
+            key = await key_selector.inner_text()
+
+            value_selector = await row.query_selector_all('span')
+            value = ', '.join([await span.inner_text()
+                               for span in value_selector])
+            # Quick hack around getting comma spearation of those spans right
+            value = value.replace(', ,', ',')
+
+            journal_metdata[key] = value
+
+        for journal_key, outputs_key in JOURNAL_SELECTORS_MAPPING.items():
+            if journal_key in journal_metdata:
+                outputs[outputs_key] = journal_metdata[journal_key]
+
+        if 'pubdate' in outputs:
+            year_match = YEAR_REGEX.match(outputs['pubdate'])
+            if year_match:
+                # Pick the capture group -- hopefully the year
+                outputs['year'] = year_match.group(1)
+
+        import pprint
+        pprint.pprint(outputs)
+        await browser.close()
+
+
+def main(url: str,
+         debug: bool = typer.Option(False),
+         proxy_server: str = typer.Option('')):
+    asyncio.run(main_extract(url, debug, proxy_server))
+
+
+if __name__ == '__main__':
+    typer.run(main)
